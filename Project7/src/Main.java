7f587180ab78f94fe40cbb8ed1d4c8ead6353cac
Patrick Remy <Patrick@Patricks-MBP.fritz.box>
2016-06-28 15:18:39 +0200

    change flowpane to gridpane
    - Add responsiveness
    - Add select exercise

--- Project7/src/Main.java
+++ Project7/src/Main.java
@@ -2,7 +2,10 @@
 import java.io.IOException;
 import java.net.URL;
 
+import gui.controllers.ExcercisesController;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,17 +15,23 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
+		primaryStage.setTitle("TDDT");
+		primaryStage.setMinWidth(600);
+		primaryStage.setMinHeight(400);
+		
 		try {
-			URL url = getClass().getResource("/gui/views/ExcercisesView.fxml");
-			Parent root = FXMLLoader.load(url);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/views/ExcercisesView.fxml"));	
+			Parent root = loader.load();
 			Scene scene = new Scene(root, 900, 600);
 			primaryStage.setScene(scene);
-	        primaryStage.setTitle("TDDT");
-	        primaryStage.show();
+			ExcercisesController controller = loader.getController();
+			controller.initWithStage(primaryStage);
 		} catch (IOException e) {
 			// TODO Handle exception
 			e.printStackTrace();
 		}
+		
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
--- Project7/src/gui/controllers/ExcercisesController.java
+++ Project7/src/gui/controllers/ExcercisesController.java
@@ -3,18 +3,27 @@ package gui.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.*;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
 import models.*;
 
 public class ExcercisesController implements Initializable {
 	
 	@FXML
-	FlowPane excercisesFlowPane;
+	GridPane excercisesPane;
 	Catalog catalog;
+	Stage stage;
 	
 	public ExcercisesController() {
 		Excercise excercise1 = new Excercise(
@@ -43,11 +52,36 @@ public class ExcercisesController implements Initializable {
 		
 		catalog = new Catalog("Test Catalog", excercise1, excercise2, excercise3);
 	}
-
-
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		for(Excercise excercise : catalog.excercises) {
+		
+	}
+	
+	public void initWithStage(Stage stage) {
+		this.stage = stage;
+		stage.widthProperty().addListener(new ChangeListener<Number>() {
+		    @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
+		    	arrangeExcercises();
+		    }
+		});
+	}
+	
+	private void arrangeExcercises() {
+		int columnsNumber = 1;
+		double width = stage.getWidth();
+		if(width > 600)
+			columnsNumber = 2;
+		if(width > 900)
+			columnsNumber = 3;
+		
+		excercisesPane.getChildren().clear();
+		excercisesPane.getColumnConstraints().clear();
+		excercisesPane.getRowConstraints().clear();
+		
+		for(int i = 0; i < catalog.excercises.size(); i++) {
+			Excercise excercise = catalog.excercises.get(i);
+			
 			String excerciseName = (excercise.name.length() > 50) ? excercise.name.substring(0, 49).trim() + "..." : excercise.name.trim();
 			Label header = new Label(excerciseName);
 			header.getStyleClass().add("excercise-header");
@@ -55,13 +89,28 @@ public class ExcercisesController implements Initializable {
 			String excerciseDescription = (excercise.description.length() > 100) ? excercise.description.substring(0, 99).trim() + "..." : excercise.description.trim();
 			Text description = new Text(excerciseDescription);
 			description.getStyleClass().add("excercise-description");
+			TextFlow descriptionFlow = new TextFlow(description);
 			
 			GridPane tile = new GridPane();
 			tile.getStyleClass().add("excercise-tile");
 			tile.add(header, 0, 0);
-			tile.add(description, 0, 1);
-			excercisesFlowPane.getChildren().add(tile);
+			tile.add(descriptionFlow, 0, 1);
+			tile.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+		          public void handle(MouseEvent e) {
+		            selectExcercise(excercise);
+		          }
+		        });
+			excercisesPane.add(tile, i % columnsNumber, i / columnsNumber);
+			// TODO: tile fill full column excercisesPane.setFillWidth(tile, true);
 		}
+		/* TODO: All columns have to have the same width
+		for(ColumnConstraints column : excercisesPane.getColumnConstraints()) {
+			column.setPercentWidth(100.0 / columnsNumber);
+		}
+		*/
 	}
 	
+	private void selectExcercise(Excercise excercise) {
+		// TODO: Implement select excercise 
+	}
 }
