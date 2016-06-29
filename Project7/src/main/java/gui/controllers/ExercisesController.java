4aca97e600846084e4591c6fbbeac2d33177a91b
Patrick Remy <Patrick@Patricks-MacBook-Pro-2.local>
2016-06-29 11:37:05 +0200

    Add select and back buttons to detail view

--- Project7/src/main/java/gui/controllers/ExercisesController.java
+++ Project7/src/main/java/gui/controllers/ExercisesController.java
@@ -5,14 +5,18 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
 import javafx.fxml.*;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
@@ -26,6 +30,7 @@ public class ExercisesController implements Initializable {
 	GridPane exercisesGrid;
 	Catalog catalog;
 	Stage stage;
+	boolean inDetailView;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -67,6 +72,10 @@ public class ExercisesController implements Initializable {
 	 */
 	private void arrangeExercises() {
 		
+		// Only arrange, if in overview
+		if(inDetailView)
+			return;
+		
 		// Get number of columns from width
 		int columnsNumber = 1;
 		double width = stage.getWidth();
@@ -75,10 +84,14 @@ public class ExercisesController implements Initializable {
 		if (width > 900)
 			columnsNumber = 3;
 
+		// TODO only arrange, if not equal columnsNumber
+		
+		// Clear grid
 		exercisesGrid.getChildren().clear();
 		exercisesGrid.getColumnConstraints().clear();
 		exercisesGrid.getRowConstraints().clear();
 		
+		// Calc percentage width and set it for each column 
 		for(int i = 0; i < columnsNumber; i++) {
 			ColumnConstraints column = new ColumnConstraints();
 			column.setPercentWidth(100.0 / columnsNumber);
@@ -108,9 +121,8 @@ public class ExercisesController implements Initializable {
 					selectExercise(exercise);
 				}
 			});
+			
 			exercisesGrid.add(tile, i % columnsNumber, i / columnsNumber);
-			// TODO: tile fill full column excercisesPane.setFillWidth(tile,
-			// true);
 		}
 	}
 
@@ -129,17 +141,36 @@ public class ExercisesController implements Initializable {
 		Text description = new Text(exercise.getDescription().trim());
 		description.getStyleClass().add("exercise-description");
 		TextFlow descriptionFlow = new TextFlow(description);
+		
+		Button selectButton = new Button("Select");
+		selectButton.setAlignment(Pos.BOTTOM_RIGHT);
+		Button backButton = new Button("Return to overview");
+		backButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                inDetailView = false;
+                arrangeExercises();
+            }
+        });
+		backButton.setAlignment(Pos.BOTTOM_LEFT);
+		GridPane.setHgrow(backButton, Priority.ALWAYS);
+		GridPane.setVgrow(backButton, Priority.ALWAYS);
 
 		GridPane tile = new GridPane();
 		tile.getStyleClass().add("exercise-tile");
 		tile.getStyleClass().add("exercise-tile-detail");
 		tile.add(header, 0, 0);
 		tile.add(descriptionFlow, 0, 1);
+		tile.add(backButton, 0, 2);
+		tile.add(selectButton, 1, 2);
 		tile.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent e) {
 				selectExercise(exercise);
 			}
 		});
+		
 		exercisesGrid.add(tile, 0, 0);
+		GridPane.setHgrow(tile, Priority.ALWAYS);
+		GridPane.setVgrow(tile, Priority.ALWAYS);
 	}
 }
