16f60005b27166c54c9a62f6f50e86ff4e2f45a7
Patrick Remy <Patrick@Patricks-MBP-2.fritz.box>
2016-06-29 16:16:15 +0200

    Add Menu

--- Project7/src/main/java/gui/controllers/MainViewController.java
+++ Project7/src/main/java/gui/controllers/MainViewController.java
@@ -7,6 +7,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -16,24 +17,35 @@ import javafx.scene.layout.Pane;
 public class MainViewController implements Initializable {
 	
 	@FXML
-	AnchorPane menuSection;
+	AnchorPane sideSection;
 	@FXML
 	AnchorPane mainSection;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/views/ExercisesView.fxml"));
-			Parent exerciseView = loader.load();
+			Parent exerciseView = FXMLLoader.load(getClass().getResource("/gui/views/ExercisesView.fxml"));
 			mainSection.getChildren().add(exerciseView);
-			AnchorPane.setTopAnchor(exerciseView, 0.0);
-			AnchorPane.setLeftAnchor(exerciseView, 0.0);
-			AnchorPane.setRightAnchor(exerciseView, 0.0);
-			AnchorPane.setBottomAnchor(exerciseView, 0.0);
+			setAllAnchorsNull(exerciseView);
+			
+			Parent menuView = FXMLLoader.load(getClass().getResource("/gui/views/MenuView.fxml"));
+			sideSection.getChildren().add(menuView);
+			setAllAnchorsNull(menuView);
 		} catch (IOException e) {
 			// TODO Handle exception
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Sets all anchors null
+	 * @param node
+	 */
+	private void setAllAnchorsNull(Node node) {
+		AnchorPane.setTopAnchor(node, 0.0);
+		AnchorPane.setLeftAnchor(node, 0.0);
+		AnchorPane.setRightAnchor(node, 0.0);
+		AnchorPane.setBottomAnchor(node, 0.0);
 	}
 
 }
