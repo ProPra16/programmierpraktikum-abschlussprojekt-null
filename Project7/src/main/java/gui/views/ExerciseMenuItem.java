8b6ac79508cb1b076aacd47e5c723e16218a96b1
Patrick Remy <Patrick@Patricks-MBP-2.fritz.box>
2016-06-30 19:01:16 +0200

    create MenuItem views
    - Create MenuItem, ExerciseMenuItem, OverviewMenuItem

---
+++ Project7/src/main/java/gui/views/ExerciseMenuItem.java
@@ -0,0 +1,38 @@
+package gui.views;
+
+import models.Exercise;
+
+public class ExerciseMenuItem extends MenuItem {
+	
+	Exercise exercise;
+	
+	/**
+	 * Constructs a menu item from an {@link Exercise}
+	 * @param exercise
+	 */
+	public ExerciseMenuItem(Exercise exercise) {
+		super();
+		
+		this.exercise = exercise;
+		setIcon(iconPencilPath);
+		setLabel(exercise.getName());
+	}
+
+	/**
+	 * Returns the {@link Exercise}
+	 * @return 
+	 */
+	public Exercise getExercise() {
+		return exercise;
+	}
+
+	/**
+	 * Sets the {@link Exercise} and renames the label
+	 * @param exercise
+	 */
+	public void setExercise(Exercise exercise) {
+		this.exercise = exercise;
+		setLabel(exercise.getName());
+	}
+	
+}
